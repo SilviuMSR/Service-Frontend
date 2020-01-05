d789a76e71a52a6472044d16e569893cc42f297d
silviumsr <manzursilviu@yahoo.com>
2020-01-05 17:07:17 +0200

    top navbar is done, change between tabs and routes and added class to selected tab

--- src/components/common/TopNavbar.js
+++ src/components/common/TopNavbar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { withStyles, Typography, AppBar, Button, IconButton, Toolbar } from '@material-ui/core'
+import { withStyles, Typography, AppBar, Button, IconButton, Toolbar, Tab, Tabs } from '@material-ui/core'
 
 const styles = theme => ({
     root: {
         flexGrow: 1,
+        backgroundColor: 'red'
+    },
+    clickedOption: {
+        color: '#1976d2',
+        borderBottom: '2px solid #1976d2'
     },
     menuButton: {
         marginRight: 2,
@@ -16,6 +21,16 @@ const styles = theme => ({
 })
 
 class TopNavbar extends Component {
+
+    state = {
+        selectedOption: ""
+    }
+
+    componentDidMount() {
+        if (this.props.history.location.pathname !== "/") {
+            this.handleClickOption(this.props.history.location.pathname.split('/')[1])
+        }
+    }
 
     renderLoginSection = () => {
         if (this.props.login.isLogged) {
@@ -30,16 +45,23 @@ class TopNavbar extends Component {
 
     }
 
+    handleClickOption = option => {
+        this.setState({ selectedOption: option }, () => this.props.history.push(`/${option}`))
+    }
+
     render() {
 
         const { classes } = this.props
 
         return (
             <div className={classes.root}>
-                <AppBar position="static">
+                <AppBar position="static" color="default">
                     <Toolbar>
                         <Typography variant="h6" className={classes.title}>
-                            <Button onClick={() => this.props.history.push('/reservations')}>Reservations</Button>
+                            <Tabs>
+                                <Tab className={this.state.selectedOption === "" ? classes.clickedOption : ""} label="Homepage" onClick={() => this.handleClickOption("")} />
+                                <Tab className={this.state.selectedOption === "reservations" ? classes.clickedOption : ""} label="Reservations" onClick={() => this.handleClickOption("reservations")} />
+                            </Tabs>
                         </Typography>
                         {this.renderLoginSection()}
                     </Toolbar>
